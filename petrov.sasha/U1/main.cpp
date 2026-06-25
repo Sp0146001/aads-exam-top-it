8cc108f50e68338700b977a26f5eda3c7bd05bb4
sp0146001 <sp0146001@gmail.com>
2026-06-25 11:48:30 +0300

    CG in person.cpp + parsing argments(files) in main structure

--- petrov.sasha/U1/main.cpp
+++ petrov.sasha/U1/main.cpp
@@ -8,11 +8,46 @@
 
 namespace petrov
 {
+  const size_t INITIAL_CAPACITY = 16;
+  static bool parseArgs(int argc, char* argv[], std::string& inFile, std::string& outFile);
 
 }
 
 int main(int argc, char* argv[])
 {
   
+}
+
+static bool petrov::parseArgs(int argc, char* argv[], std::string& inFile, std::string& outFile)
+{
+  bool hasIn = false;
+  bool hasOut = false;
+  for (int i = 1; i < argc; ++i)
+  {
+    std::string arg = argv[i];
+    if (arg.substr(0, 3) == "in:")
+    {
+      if (hasIn)
+      {
+        return false;
+      }
+      hasIn = true;
+      inFile = arg.substr(3);
+    }
+    else if (arg.substr(0, 4) == "out:")
+    {
+      if (hasOut)
+      {
+        return false;
+      }
+      hasOut = true;
+      outFile = arg.substr(4);
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
