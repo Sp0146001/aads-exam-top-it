b722b872651f5c63f677cb5755e87f98172c03cb
sp0146001 <sp0146001@gmail.com>
2026-06-25 12:18:49 +0300

    CG 10 - remove const from size_t type

--- petrov.sasha/U1/person.hpp
+++ petrov.sasha/U1/person.hpp
@@ -23,11 +23,10 @@ namespace petrov
   personArr* createPersonArray(size_t capacity);
   void destroyPersonArray(personArr* array);
   void pushPerson(personArr* array, const Person& person);
-  bool isDuplicate(const personArr* array, const size_t id);
+  bool isDuplicate(const personArr* array, size_t id);
   bool parseLine(const std::string& line, Person& person);
   void readPersons(std::istream& inStream, personArr* persons, size_t& successCount, size_t& ignoredCount);
   void writePersons(std::ostream& outStream, const personArr* persons);
 }
 
 #endif
-
