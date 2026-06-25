6aa78ed0d6c4544dbd41e98097247b25e9931733
sp0146001 <sp0146001@gmail.com>
2026-06-25 14:15:08 +0300

    update commands

--- petrov.sasha/U2/commands.cpp
+++ petrov.sasha/U2/commands.cpp
@@ -70,15 +70,42 @@ namespace
     }
   }
 
-  void commandAnons(petrov::DataBase& db)
+  void commandAnons(petrov::DataBase & db)
   {
+    std::size_t count = 0;
     for (std::size_t i = 0; i < db.persons.size; ++i)
     {
       if (!db.persons.data[i].hasInfo)
       {
-        std::cout << db.persons.data[i].id << '\n';
+        ++count;
       }
     }
+    std::size_t * temp = new std::size_t[count];
+    std::size_t index = 0;
+    for (std::size_t i = 0; i < db.persons.size; ++i)
+    {
+      if (!db.persons.data[i].hasInfo)
+      {
+        temp[index++] = db.persons.data[i].id;
+      }
+    }
+    for (std::size_t i = 0; i < count; ++i)
+    {
+      for (std::size_t j = 0; j + 1 < count; ++j)
+      {
+        if (temp[j] > temp[j + 1])
+        {
+          std::size_t t = temp[j];
+          temp[j] = temp[j + 1];
+          temp[j + 1] = t;
+        }
+      }
+    }
+    for (std::size_t i = 0; i < count; ++i)
+    {
+      std::cout << temp[i] << '\n';
+    }
+    delete[] temp;
   }
 
   void commandMeets(petrov::DataBase& db)
@@ -89,26 +116,34 @@ namespace
       std::cout << "<INVALID COMMAND>\n";
       return;
     }
-    bool found = false;
+    std::size_t index = 0;
+    if (!findPersonIndex(db, id, index))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return;
+    }
+    petrov::Meeting* temp = new petrov::Meeting[db.meetings.size];
+    std::size_t count = 0;
     for (std::size_t i = 0; i < db.meetings.size; ++i)
     {
-      petrov::Meeting& m = db.meetings.data[i];
-
+      petrov::Meeting m = db.meetings.data[i];
       if (m.first == id)
       {
-        std::cout << m.second << ' ' << m.time << '\n';
-        found = true;
+        m.first = m.second;
+        temp[count++] = m;
       }
       else if (m.second == id)
       {
-        std::cout << m.first << ' ' << m.time << '\n';
-        found = true;
+        temp[count++] = m;
       }
     }
-    if (!found)
+    sortMeetings(temp, count);
+    for (std::size_t i = 0; i < count; ++i)
     {
-      std::cout << "<INVALID COMMAND>\n";
+      std::cout << temp[i].first << ' ' << temp[i].time << '\n';
     }
+
+    delete[] temp;
   }
 }
 
