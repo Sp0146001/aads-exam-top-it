2e7afdbdb2cb59fbfff83fe04a36b2dad4095659
sp0146001 <sp0146001@gmail.com>
2026-06-25 13:53:29 +0300

    initialization and import data - test for working archtechture

--- petrov.sasha/U2/main.cpp
+++ petrov.sasha/U2/main.cpp
@@ -1,15 +1,47 @@
 #include "domain.hpp"
+#include "input_args.hpp"
+#include "io.hpp"
+
 #include <iostream>
 
-int main()
+int main(int argc, char * argv[])
 {
-  petrov::DataBase db =
+  try
   {
-    { nullptr, 0, 0 },
-    { nullptr, 0, 0 }
-  };
-  petrov::initDataBase(db);
-  petrov::destroyDataBase(db);
-
-  return 0;
+    petrov::ProgramOptions options = {};
+    if (!petrov::parseArguments(argc, argv, options))
+    {
+      std::cerr << "Invalid command line arguments\n";
+      return 1;
+    }
+    petrov::DataBase db =
+    {
+      { nullptr, 0, 0 },
+      { nullptr, 0, 0 }
+    };
+    petrov::initDataBase(db);
+    if (options.hasPersonsFile)
+    {
+      if (!petrov::readPersonsFromFile(options.personsFile, db))
+      {
+        std::cerr << "Cannot open persons file\n";
+        petrov::destroyDataBase(db);
+        return 2;
+      }
+    }
+    int result = petrov::readMeetingsFromFile(options.dataFile, db);
+    if (result != 0)
+    {
+      std::cerr << "Invalid meetings data\n";
+      petrov::destroyDataBase(db);
+      return result;
+    }
+    petrov::destroyDataBase(db);
+    return 0;
+  }
+  catch (...)
+  {
+    std::cerr << "Internal error\n";
+    return 2;
+  }
 }
