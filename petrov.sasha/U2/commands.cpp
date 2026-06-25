2f26fa6b7ae15e3b422ffe5095ae48c0a133a8f4
sp0146001 <sp0146001@gmail.com>
2026-06-25 14:05:13 +0300

    commands - desc, anons, meets. Else - INVALID CMD

---
+++ petrov.sasha/U2/commands.cpp
@@ -0,0 +1,108 @@
+#include "commands.hpp"
+
+#include <iostream>
+
+namespace
+{
+  bool findPersonIndex(const petrov::DataBase& db, std::size_t id, std::size_t& index)
+  {
+    for (std::size_t i = 0; i < db.persons.size; ++i)
+    {
+      if (db.persons.data[i].id == id)
+      {
+        index = i;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void commandDesc(petrov::DataBase& db)
+  {
+    std::size_t id = 0;
+    if (!(std::cin >> id))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return;
+    }
+    std::size_t index = 0;
+    if (!findPersonIndex(db, id, index))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return;
+    }
+    if (db.persons.data[index].hasInfo)
+    {
+      std::cout << db.persons.data[index].info << '\n';
+    }
+    else
+    {
+      std::cout << "<ANON>\n";
+    }
+  }
+
+  void commandAnons(petrov::DataBase& db)
+  {
+    for (std::size_t i = 0; i < db.persons.size; ++i)
+    {
+      if (!db.persons.data[i].hasInfo)
+      {
+        std::cout << db.persons.data[i].id << '\n';
+      }
+    }
+  }
+
+  void commandMeets(petrov::DataBase& db)
+  {
+    std::size_t id = 0;
+    if (!(std::cin >> id))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return;
+    }
+    bool found = false;
+    for (std::size_t i = 0; i < db.meetings.size; ++i)
+    {
+      petrov::Meeting& m = db.meetings.data[i];
+
+      if (m.first == id)
+      {
+        std::cout << m.second << ' ' << m.time << '\n';
+        found = true;
+      }
+      else if (m.second == id)
+      {
+        std::cout << m.first << ' ' << m.time << '\n';
+        found = true;
+      }
+    }
+    if (!found)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
+
+void petrov::commandLoop(DataBase& db)
+{
+  std::string command = "";
+  while (std::cin >> command)
+  {
+    if (command == "desc")
+    {
+      commandDesc(db);
+    }
+    else if (command == "anons")
+    {
+      commandAnons(db);
+    }
+    else if (command == "meets")
+    {
+      commandMeets(db);
+    }
+    else
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
